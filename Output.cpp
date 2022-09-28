0aa2d9ef489446be9c9b6f4f159c252207ffcfa1
VIBHAVRI99 <urvishrivas99@gmail.com>
2022-09-28 20:30:57 +0530

    t

---
+++ Output.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+
+{
+	
+	printf("Vibhavri Shrivas");
+	return 0;
+}
---
+++ variable.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() 
+{
+  int Num = 15;
+  printf(=%i; Num);
+  return 0;
+}
